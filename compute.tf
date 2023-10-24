61ea90943809fa9602f16434f630663f4414a355
Tony Bo <tonykhbo@gmail.com>
2023-10-24 16:09:58 +0000

    New Export for 2023-10-24_16-09-49

--- compute.tf
+++ compute.tf
@@ -1,76 +1,40 @@
-resource "databricks_cluster" "shasidhar_dev_cluster_0810_094321_3j8qu522" {
+resource "databricks_cluster" "repos_git_proxy_1024_154128_a6jt5yv8" {
   spark_version = "12.2.x-scala2.12"
-  spark_env_vars = {
-    PYSPARK_PYTHON = "/databricks/python3/bin/python3"
-  }
   spark_conf = {
-    "spark.databricks.cluster.profile"       = "singleNode"
-    "spark.databricks.delta.preview.enabled" = "true"
-    "spark.master"                           = "local[*, 4]"
+    "spark.databricks.cluster.profile" = "singleNode"
+    "spark.master"                     = "local[*]"
   }
-  single_user_name    = "shasidhar.eranti@databricks.com"
-  runtime_engine      = "STANDARD"
   node_type_id        = "Standard_DS3_v2"
   enable_elastic_disk = true
-  data_security_mode  = "LEGACY_SINGLE_USER_STANDARD"
   custom_tags = {
     ResourceClass = "SingleNode"
   }
-  cluster_name = "Shasidhar dev cluster"
+  cluster_name = "Repos Git Proxy"
   azure_attributes {
     spot_bid_max_price = -1
     first_on_demand    = 1
     availability       = "ON_DEMAND_AZURE"
   }
 }
-resource "databricks_cluster" "shasidhar_eranti_s_cluster_0810_115652_38i8xxjb" {
-  spark_version = "12.2.x-scala2.12"
+resource "databricks_cluster" "tony_bo_s_cluster_1024_153625_ju97z4me" {
+  spark_version = "13.3.x-scala2.12"
   spark_env_vars = {
     PYSPARK_PYTHON = "/databricks/python3/bin/python3"
-  }
-  spark_conf = {
-    "spark.databricks.delta.preview.enabled" = "true"
-  }
-  single_user_name    = "shasidhar.eranti@databricks.com"
-  runtime_engine      = "STANDARD"
-  node_type_id        = "Standard_DS3_v2"
-  enable_elastic_disk = true
-  data_security_mode  = "SINGLE_USER"
-  cluster_name        = "Shasidhar Eranti's Cluster"
-  azure_attributes {
-    spot_bid_max_price = -1
-    first_on_demand    = 1
-    availability       = "ON_DEMAND_AZURE"
-  }
-  autotermination_minutes = 120
-  autoscale {
-    min_workers = 1
-    max_workers = 8
-  }
-}
-resource "databricks_cluster" "tony_bo_s_cluster_0725_192405_4t5nwqc" {
-  spark_version = "12.2.x-scala2.12"
-  spark_env_vars = {
-    PYSPARK_PYTHON = "/databricks/python3/bin/python3"
-  }
-  spark_conf = {
-    "spark.databricks.cluster.profile"       = "singleNode"
-    "spark.databricks.delta.preview.enabled" = "true"
-    "spark.master"                           = "local[*, 4]"
   }
   single_user_name    = "tony.bo@databricks.com"
-  runtime_engine      = "STANDARD"
+  runtime_engine      = "PHOTON"
   node_type_id        = "Standard_DS3_v2"
   enable_elastic_disk = true
   data_security_mode  = "LEGACY_SINGLE_USER_STANDARD"
-  custom_tags = {
-    ResourceClass = "SingleNode"
-  }
-  cluster_name = "Tony Bo's Cluster"
+  cluster_name        = "Tony Bo's Cluster"
   azure_attributes {
     spot_bid_max_price = -1
     first_on_demand    = 1
     availability       = "ON_DEMAND_AZURE"
   }
-  autotermination_minutes = 10
+  autotermination_minutes = 20
+  autoscale {
+    min_workers = 2
+    max_workers = 8
+  }
 }
